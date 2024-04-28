ea69cd077d9f744d3a8965a5fd3ad484d88246be
isamu <isamu@to-kyo.to>
2024-04-28 12:14:28 +0900

    args

--- src/graphai_cli.ts
+++ src/graphai_cli.ts
@@ -2,21 +2,37 @@
 
 import { GraphAI } from "graphai";
 import * as agents from "graphai/lib/experimental_agents";
-import yargs from 'yargs';
+import yargs from "yargs";
 
 import fs from "fs";
 import path from "path";
 import YAML from "yaml";
 
+const hasOption = process.argv.includes("-l") || process.argv.includes("--list");
+
 const args = yargs
-  .command("* <yaml_file>", "run GraphAI with yaml GraphAI file.")
-  .parseSync()
+  .option("list", {
+    alias: "l",
+    description: "agents list",
+  })
+  .command(hasOption ? "* [yaml_file]" : "* <yaml_file>", "run GraphAI with yaml GraphAI file.")
+  .parseSync();
 
 const main = async () => {
+  if (args.list) {
+    console.log("Available Agents");
+    console.log(
+      Object.keys(agents)
+        .sort()
+        .map((a) => "- " + a)
+        .join("\n"),
+    );
+    return;
+  }
   const file = args.yaml_file;
   const file_path = path.resolve(process.cwd() + "/" + file);
   if (!fs.existsSync(file_path)) {
-    console.log("no file");
+    console.log("no file exist: " + file_path);
     return;
   }
   try {
