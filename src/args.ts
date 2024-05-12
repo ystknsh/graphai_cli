9685b1934ead2d9c9ea6690e8a736cba432f585e
isamu <isamu@to-kyo.to>
2024-05-12 13:42:43 +0900

    add scriptname

--- src/args.ts
+++ src/args.ts
@@ -3,6 +3,7 @@ import yargs from "yargs";
 export const hasOption = ["-l", "--list", "-d", "--detail", "-s", "--sample"].some((o) => process.argv.includes(o));
 
 export const args = yargs
+  .scriptName('graphai')
   .option("list", {
     alias: "l",
     description: "agents list",
